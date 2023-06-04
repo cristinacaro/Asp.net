14326b89924dd626cb3b3cb5fff544e7fa8c79f4
Cristina Caro Muñoz <cristina.caro.90@gmail.com>
2023-06-03 20:17:56 -0400

    Login
    Se realizo la conexion a la base de datos, controlador y modelo.

---
+++ Proyecto/Proyecto/Controllers/InicioController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+using System.Data.SqlClient;
+
+
+namespace Proyecto.Controllers
+{
+    public class InicioController : Controller
+    {
+
+        // Cadena de conexion a la bd
+        static string cadena = "server=DESKTOP-O59N4G8\\SQLEXPRESS ; database=Prueba ; integrated security = true";
+
+        // GET: Inicio
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Usuario usuario)
+        {
+            
+
+           using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                connection.Open();
+
+                string sqlQuery = "SELECT COUNT(*) FROM usuario WHERE email= '@email' AND password = '@password'";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    
+                    command.Parameters.AddWithValue("@email", usuario.email);
+                    command.Parameters.AddWithValue("@password",usuario.password);
+
+                    usuario.idUsuario = (int)command.ExecuteScalar();
+
+                  
+                }
+
+                connection.Close();
+            }
+
+            if (usuario.idUsuario !=0)
+            {
+                Session["usuario"] = usuario;
+                return RedirectToAction("Index", "Home");
+
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                ViewData["Mensaje"] = "usuario no encontrado";
+              
+            }
+
+         
+        }
+
+
+     
+    
+      
+    }
+    }
---
+++ Proyecto/Proyecto/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+
+namespace Proyecto.Models
+{
+    
+    public class Usuario
+    {
+        public int  idUsuario { get; set; }
+      
+        public string  Nombre { get; set; }
+        
+        public int telefono { get; set; }
+       
+        public string direccion { get; set; }
+    
+        public string email { get; set; }
+        
+        public string password { get; set; }
+
+
+    }
+}
